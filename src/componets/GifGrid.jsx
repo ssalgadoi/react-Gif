863f29b3bc8a509c406c76ac0e4c562b5ce137e9
Sebastián <se.salgado@duocuc.cl>
2024-02-12 20:25:00 -0300

    Trabajamos con useEffect

--- src/componets/GifGrid.jsx
+++ src/componets/GifGrid.jsx
@@ -1,16 +1,22 @@
+import { useState, useEffect } from "react";
 import { getGif } from "../helpers/getGifs";
+
 
 
 export const GifGrid = ( { category } ) => {
 
-  
+  const [ counter, setCounter] = useState(10);
+  useEffect( () => {
+    getGif( category );
+  }, [ ])
+
   getGif( category );
   
-
-
   return (
     <>
     <h3>{ category }</h3>
+    <h5>{ counter }</h5>
+    <button onClick={ () => setCounter( counter + 1) }>+1</button>
     </>
   )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGif = async( category ) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=ONJCHSRVCPylNlfSLukBRGIOhdjCQ2rJ&q=${ category }&limit=20`
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=ONJCHSRVCPylNlfSLukBRGIOhdjCQ2rJ&q=${ category }&limit=10`
   const resp = await fetch( url );
   const { data } = await resp.json();
 
